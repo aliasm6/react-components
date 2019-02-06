f5a23c5cd8d69b78e4d95c5682d18ccdde8b0a40
Alias M6 <aliasmontoya719@gmail.com>
2019-02-05 22:42:01 -0700

    MAKE MUlTIPLE COMMENTS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 const App = () => {
-  return <div>Hi there</div>
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span class="date">Today at 6PM</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span class="date">Today at 6PM</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="comment">
+        <a href="/" className="avatar">
+        <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span class="date">Today at 6PM</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
