902be447c258794a0ffe78d53f9ecc0365d9f282
Alias M6 <aliasmontoya719@gmail.com>
2019-02-12 19:39:34 -0700

    REFACTOR COMMENT DETAIL COMPONENT TO RECEIVE USER COMMENT PROP

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Laticia" />
-      <CommentDetail author="Jimon" />
-      <CommentDetail author="Amreet" />
+      <CommentDetail author="Laticia" timeAgo="Today at 5:45PM" userComment="This is great"   />
+      <CommentDetail author="Jimon" timeAgo="Today at 7:36AM" userComment="You're awesome"  />
+      <CommentDetail author="Amreet" timeAgo="Yesterday at 11:59PM" userComment="I whole-heartedly disagree"  />
     </div>
   );
 }
