3a34702eb46998282b158ecabc27e6d8d45d323c
Alias M6 <aliasmontoya719@gmail.com>
2019-02-12 20:28:01 -0700

    CREATE APPROVAL CARD COMPONENT'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Laticia" timeAgo="Today at 5:45PM" userComment="This is great"   />
-      <CommentDetail author="Jimon" timeAgo="Today at 7:36AM" userComment="You're awesome"  />
-      <CommentDetail author="Amreet" timeAgo="Yesterday at 11:59PM" userComment="I whole-heartedly disagree"  />
+      <ApprovalCard />
+      <CommentDetail
+        author="Laticia" 
+        timeAgo="Today at 5:45PM"
+        userComment="This is great"
+      />
+      <CommentDetail
+        author="Jimon"
+        timeAgo="Today at 7:36AM"
+        userComment="You're awesome"
+      />
+      <CommentDetail
+        author="Amreet"
+        timeAgo="Yesterday at 11:59PM"
+        userComment="I whole-heartedly disagree"
+      />
     </div>
+
   );
 }
 
