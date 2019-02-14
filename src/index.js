c35087108d2e22a93cdd58464f7cee052c4d92bd
Alias M6 <aliasmontoya719@gmail.com>
2019-02-13 22:23:49 -0700

    ADD INSTANCE OF APPROVAL CARD TO APP TO EXPERIMENT WITH USABILITY

--- src/index.js
+++ src/index.js
@@ -6,22 +6,30 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
   return (
     <div className="ui container comments">
-      <ApprovalCard />
-      <CommentDetail
-        author="Laticia" 
-        timeAgo="Today at 5:45PM"
-        userComment="This is great"
-      />
-      <CommentDetail
-        author="Jimon"
-        timeAgo="Today at 7:36AM"
-        userComment="You're awesome"
-      />
-      <CommentDetail
-        author="Amreet"
-        timeAgo="Yesterday at 11:59PM"
-        userComment="I whole-heartedly disagree"
-      />
+      <ApprovalCard>
+        Are you sure you want to do that? 
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Laticia"
+          timeAgo="Today at 5:45PM"
+          userComment="This is great"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jimon"
+          timeAgo="Today at 7:36AM"
+          userComment="You're awesome"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Amreet"
+          timeAgo="Yesterday at 11:59PM"
+          userComment="I whole-heartedly disagree"
+        />
+      </ApprovalCard>
     </div>
 
   );
